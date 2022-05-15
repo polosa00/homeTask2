12ead1c5b86fb5a15a527013b42c65e0ef19b18d
polosa00 <i@apolochanin.ru>
2022-05-16 00:35:20 +0300

    Initial Commit

---
+++ homeTask2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homeTask2
+//
+//  Created by Institute Sk on 16.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
