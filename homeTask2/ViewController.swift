5773fb0b96f03ebd4d4460bbc0eb11f13e390020
polosa00 <i@apolochanin.ru>
2022-05-16 01:07:57 +0300

    Initial commit

--- homeTask2/ViewController.swift
+++ homeTask2/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainView: UIView!
+    @IBOutlet var labelValueRedSlider: UILabel!
+    @IBOutlet var labelValueGreenSlider: UILabel!
+    @IBOutlet var labelValueBlueSlider: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func actionRS(_ sender: Any) {
+    }
+    
+    @IBAction func actionGS(_ sender: Any) {
+    }
+    @IBAction func actionBS(_ sender: Any) {
+    }
 }
 
